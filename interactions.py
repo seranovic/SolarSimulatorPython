ce6b592b572bf71264391b3445e797efaee8fe77
seraphim <ssseraphim@proton.me>
2026-02-23 10:31:50 +0100

    initial commit

---
+++ interactions.py
@@ -0,0 +1,17 @@
+'''
+This is where the interactions module lives e.g. the F in F = m*a
+Currently this is an O(N^2) approach.
+'''
+
+def get_forces(pos, mass):
+    forces = np.zeros_like(pos)
+    for idx in range(len(forces)):
+        vectors = pos - pos[idx]
+        distances_row = np.sum(vectors**2, axis=1)**0.5
+        distances = distances_row[:, np.newaxis]
+        distances[idx] = np.inf
+        prefactors = gravitational_constant*mass*mass[idx]/distances**3
+        pair_force = prefactors*vectors
+        total_force = np.sum(pair_force, axis=0)
+        forces[idx] = total_force
+    return forces
