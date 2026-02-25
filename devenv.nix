cca17fe89f1a7750f4001362ca9e3bb9726f75b1
Nuhddy <noahpc@live.dk>
2026-02-26 00:56:15 +0100

    add devenv stuff

---
+++ devenv.nix
@@ -0,0 +1,10 @@
+{...}: {
+  languages.python = {
+    enable = true;
+    venv = {
+      enable = true;
+      requirements = builtins.readFile ./requirements.txt;
+    };
+  };
+  cachix.enable = false;
+}
