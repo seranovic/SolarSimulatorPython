29379e794ca3a86538c76e100494955f74d4b28e
seraphim <ssseraphim@proton.me>
2026-03-24 14:55:46 +0100

    updated leapfrog method to work

--- integrators.py
+++ integrators.py
@@ -1,12 +1,9 @@
 
-def LeapFrog(forces, positions, velocities, masses, time_step,):
-       x = positions[:, 0]
-       y = positions[:, 1]
-       forces = get_forces(positions, masses)
-       accelerations = forces / masses
-       velocities = velocities + accelerations * time_step
-       positions = positions + velocities * time_step
-       time = time + time_step
+def LeapFrog(forces, pos, vel, mass, dt):
 
+       accelerations = forces / mass
+       vel = vel + accelerations * dt
+       pos = pos + vel * dt
 
+       return pos, vel
 
