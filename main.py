a0e45aeec14b80471e4925bbd6c65c06f2bb9019
seraphim <ssseraphim@proton.me>
2026-03-24 14:54:20 +0100

    added run() function that runs the program and stores data over time as a np.array

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from numba.np.arrayobj import dtype_type
+
 import interactions
 import integrators
 import numpy as np
@@ -19,54 +21,26 @@ masses = np.array([
     [1.69e30]
 ])
 
-def array_maker(pos, end_time, factor):
-    '''Creates a numpy array that stores the positions value of each celestial object.'''
-    n = int(pos.size/3) #number of objects
-    #print(n)
-    t = int(end_time/factor) #number of total timesteps
-    #print(t)
 
-    pos = np.zeros((t,n,3)) # first number is column number, e.g. time, second and third number give dimensonality
-    # we have n objects with 3 dimensions so should be t*n*3 array.
 
-    return pos
-
-def run(pos, vel, masses, steps, innersteps, force.func):
-    n, d = pos.positions.shape
-    pos_t = np.zeroes(steps,n,d)
+def run(pos, vel, mass, dt, steps, innersteps, force_func):
+    n, d = pos.shape
+    pos_t = np.zeros((steps,n,d))
 
     for step in range(steps):
         for innerstep in range(innersteps):
+            forces = force_func(pos, mass)
+            pos, vel = integrators.LeapFrog(forces, pos, vel, mass, dt)
+
+        pos_t[step] = pos
+        print(pos)
+
+    return pos_t
+
 
 
 
 if __name__ == "__main__":
-    time_step = 60 * 60 * 24 # 1 day
-    time = 0
-    time_end = 5 * 60 * 60 * 24 * 365 # 5 years
-    factor = 365*60*60*24 # every year
-    plt.figure()
-    plt.axes(projection='3d')
 
-    while (time < time_end):
-        x = positions[:, 0]
-        y = positions[:, 1]
-        z = positions[:, 2]
-        forces = interactions.get_forces(positions, masses)
-        accelerations = forces / masses
-        velocities = velocities + accelerations * time_step
-        positions = positions + velocities * time_step
-        time = time + time_step
-        print(f'days are now:{time / (60 * 60 * 24)}, x:{x}, y:{y}, z:{z}')
-        print(f' modulo: {time % 365 * 60 * 60 * 24}')
-        print(f' positions np.array: {positions}')
-        if (time % factor == 0):  # plots every year.
-            plt.plot(x, y, z, 'o')
-#plt.show()
+    run(positions, velocities, masses, time_step, 100, 100, interactions.get_forces_numpy)
 
-print(array_maker(positions, time_step, time_end,factor))
-
-
-#find way to separate the integrator for easy hot-swap
-#find better way to display 3d graph.
-#find way to store data.
