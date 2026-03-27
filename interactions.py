9c4e4a04878731a9cfb96049e1f897158a3e1237
seraphim <ssseraphim@proton.me>
2026-03-27 13:47:36 +0100

    updated with new get_forces function that uses standard python.

--- interactions.py
+++ interactions.py
@@ -11,10 +11,28 @@ def get_forces_numpy(pos, mass):
     for idx in range(len(forces)):
         vectors = pos - pos[idx]
         distances_row = np.sum(vectors**2, axis=1)**0.5
-        distances = distances_row[:, np.newaxis]
+        distances = distances_row[:, np.newaxis] ## currently only works on 2d, might be fun to rewrite later.
         distances[idx] = np.inf
         prefactors = constants.gravitational_constant*mass*mass[idx]/distances**3
         pair_force = prefactors*vectors
         total_force = np.sum(pair_force, axis=0)
         forces[idx] = total_force
+    return forces
+
+def get_forces(pos, mass):
+    forces = np.zeros_like(pos)
+
+    for i in range(len(forces)):
+        for j in range(i+1,len(forces)):
+            r_vector = pos[j] - pos[i]
+            r_magnitude = np.linalg.norm(r_vector)
+            if r_magnitude == 0:
+                continue
+            prefactors = constants.gravitational_constant*mass[i]*mass[j]/r_magnitude**3
+
+            pair_force = prefactors*r_vector
+
+            forces[i] += pair_force
+            forces[j] -= pair_force
+
     return forces
