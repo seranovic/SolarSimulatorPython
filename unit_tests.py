6cfe36f9fe0a86d51d8b2858c1bab377d0e3b984
seraphim <ssseraphim@proton.me>
2026-04-29 15:47:40 +0200

    added momentum_collision_inelastic_test() unit test. currently not working need to fix the moment calc.

--- unit_tests.py
+++ unit_tests.py
@@ -3,7 +3,7 @@ from main import run
 import interactions
 import tests
 
-def momemtum_collision_test():
+def momentum_collision_elastic_test():
     positions = np.array([
         [-1, 0],
         [1, 0]
@@ -22,7 +22,38 @@ def momemtum_collision_test():
     radii = np.array([1, 1])
     momentum1 = tests.momentum_calc(velocity, mass)
 
-    data = run(positions, velocity, mass, 1, True, 0.1, 10, 10, interactions.get_forces_zeroes)
+    data = run(positions, velocity, mass, 1, 'Elastic', 0.1, 10, 10, interactions.get_forces_zeroes)
+
+    momentum2 = tests.momentum_calc(data['Velocity'][-1,:], mass)
+
+    if momentum1 == momentum2:
+        print(velocity, data['Velocity'][-1,:])
+        return True
+    else:
+        print(momentum1, momentum2)
+        print(velocity, data['Velocity'][-1,:])
+        return False
+
+def momentum_collision_inelastic_test():
+    positions = np.array([
+        [-1, 0],
+        [1, 0]
+    ], dtype=np.float64)
+
+    mass = np.array([
+        1,
+        1
+    ], dtype=np.float64)
+
+    velocity = np.array([
+        [1, 0],
+        [-1, 0]
+    ], dtype=np.float64)
+
+    radii = np.array([1, 1])
+    momentum1 = tests.momentum_calc(velocity, mass)
+
+    data = run(positions, velocity, mass, 1, 'Inelastic', 0.1, 10, 10, interactions.get_forces_zeroes)
 
     momentum2 = tests.momentum_calc(data['Velocity'][-1,:], mass)
 
@@ -35,8 +66,10 @@ def momemtum_collision_test():
         return False
 
 
+
 if __name__ == '__main__':
-    print(f'COLISSION TEST: {momemtum_collision_test()}')
+    print(f'MOMENTUM ELASTIC TEST: {momentum_collision_elastic_test()}')
+    print(f'MOMENTUM INELASTIC TEST: {momentum_collision_inelastic_test()}')
 
 
 
