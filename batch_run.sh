d8d39e7f0313c5c9831a210b89297769dabcf067
Nuhddy <noahpc@live.dk>
2026-05-18 20:20:12 +0200

    add time limit

--- batch_run.sh
+++ batch_run.sh
@@ -2,12 +2,21 @@
 #SBATCH --account DeiC-RUC-L2-202601
 #SBATCH -c 32
 #SBATCH --mem 16g
+#SBATCH -t 960
+
+# -c amount of logical cores (threads)
+# --mem amount of memory
+# -t minimum time in minutes (should be 480 per 1e4x5e4 run)
+
+runs=2
+outersteps=10000
+innersteps=50000
 
 source env/bin/activate
 
-for i in $(seq 1 3); do
+for i in $(seq 1 $runs); do
     python init_cond_2_stars.py $i
-    python run_script.py $i 10000 50000
+    python run_script.py $i $outersteps $innersteps
 done
 
 deactivate
