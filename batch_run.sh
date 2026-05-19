2065d5974d5060cca477dc13fa9517cdde2c7b87
Nuhddy <noahpc@live.dk>
2026-05-19 13:30:29 +0200

    longer time limit

--- batch_run.sh
+++ batch_run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #SBATCH --account DeiC-RUC-L2-202601
 #SBATCH -c 32
-#SBATCH --mem 16g
-#SBATCH -t 960
+#SBATCH --mem 8g
+#SBATCH -t 10080
 
 # -c amount of logical cores (threads)
 # --mem amount of memory
 # -t minimum time in minutes (should be 480 per 1e4x5e4 run)
 
-runs=2
+offset=1
+runs=20
 outersteps=10000
 innersteps=50000
 
 source env/bin/activate
 
-for i in $(seq 1 $runs); do
+for i in $(seq $((1 + $offset)) $(($runs + $offset))); do
+    echo "ID: $i"
     python init_cond_2_stars.py $i
     python run_script.py $i $outersteps $innersteps
 done
